6aa774bfda9f9a07d65d18767130fbf7cc590efc
Janviag15 <123718857+Janviag15@users.noreply.github.com>
2024-05-18 13:33:27 +0000

    Datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+//primitive datatype
+
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 656543527998765431n
+
+
+//Referance (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Janvi",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
+
+
+
+
+
+
