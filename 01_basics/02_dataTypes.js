a2568ce4c57caeeefa18e4f1bc209422c72134e4
Janviag15 <123718857+Janviag15@users.noreply.github.com>
2024-05-18 10:43:40 +0000

    DatatTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat as JS code as newer version
+
+//alert (3+3) //we are using nodejs not browser
+
+console.log(3+3); 
+
+console.log("Janvi")
+
+let name = "Janvi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof"Janvi");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
