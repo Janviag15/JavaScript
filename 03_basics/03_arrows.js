c5213376bb62a0d7f234b1b322d40043624c6b9f
Janviag15 <123718857+Janviag15@users.noreply.github.com>
2024-05-20 07:55:19 +0000

    Arrows in JS

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,50 @@
+const user = {
+    username: "Janvi",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+console.log(this);
+
+function one(){
+     let username = "Janvi"
+     console.log(this.username);
+ }
+
+one()
+
+const two = function () {
+     let username = "Janvi"
+     console.log(this.username);
+}
+two()
+
+const three =  () => {
+    let username = "Janvi"
+    console.log(this);
+}
+
+
+three()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2 //explicit return
+}
+console.log(addTwo(3,4));
+
+const add2 = (num1, num2) =>  num1 + num2 //implicit return
+console.log(add2(3,4));
+
+const add = (num1, num2) => ( num1 + num2 )
+console.log(add(3,4));
+
+//const myArr = [2,5,3,7,9]
+//myArr.forEach(() => ())
